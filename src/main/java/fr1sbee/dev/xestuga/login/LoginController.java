413bbe1d2e9149e1e2307aac052025418c5c6362
Fr1sbeeBRZ <ikeralvitealmozaravilacerio@gmail.com>
2025-05-25 20:49:54 +0200

    Cambios para Spring security

--- src/main/java/fr1sbee/dev/xestuga/login/LoginController.java
+++ src/main/java/fr1sbee/dev/xestuga/login/LoginController.java
@@ -3,6 +3,7 @@ package fr1sbee.dev.xestuga.login;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,9 +12,9 @@ public class LoginController {
     private final Logger LOGGER = LogManager.getLogger(LoginController.class);
 
 
-    @RequestMapping("")
+    @GetMapping("")
     public String index(){
         LOGGER.info("Accediendo al login ");
-        return "login.html";
+        return "login";
     }
 }
