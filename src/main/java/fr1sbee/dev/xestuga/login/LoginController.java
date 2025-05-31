944c9729f3d111e3f36a4750fd8eb0e110ecdf86
Fr1sbeeBRZ <ikeralvitealmozaravilacerio@gmail.com>
2025-05-31 12:32:25 +0200

    Conexion a base de datos

--- src/main/java/fr1sbee/dev/xestuga/login/LoginController.java
+++ src/main/java/fr1sbee/dev/xestuga/login/LoginController.java
@@ -2,21 +2,32 @@ package fr1sbee.dev.xestuga.login;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     private final Logger LOGGER = LogManager.getLogger(LoginController.class);
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("")
-    public String index(){
-        LOGGER.info("Accediendo al login ");
-        return "/home";
+    @ResponseBody
+    public String testConnection() {
+        try {
+            LOGGER.info("dentro del test");
+            Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            return "Conexión exitosa: Resultado = " + result;
+        } catch (Exception e) {
+            return "Error de conexión: " + e.getMessage();
+        }
     }
 }
--- src/main/java/fr1sbee/dev/xestuga/test/DBTestController.java
+++ src/main/java/fr1sbee/dev/xestuga/test/DBTestController.java
@@ -1,23 +1,16 @@
 package fr1sbee.dev.xestuga.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DBTestController {
+    private final Logger LOG = LogManager.getLogger(DBTestController.class);
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/dbtest")
-    public String testConnection() {
-        try {
-            Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-            return "Conexión exitosa: Resultado = " + result;
-        } catch (Exception e) {
-            return "Error de conexión: " + e.getMessage();
-        }
-    }
 }
